fba0adfc969de42364be850c9965529b4258209f
Mu Li <muli@cs.cmu.edu>
2013-12-14 04:31:38 -0500

    editing report

---
+++ apps/pagarank/bin_data.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <glog/logging.h>
+#include <string>
+
+namespace quiltdb {
+
+// manipulate binary data
+template <class T>
+static size_t load_bin(std::string const& filename, T **buf, long offset, size_t n) {
+  FILE *fp = fopen(filename.c_str(), "rb");
+  CHECK_NE(fp, NULL) <<  "open " << filename << " for reading failed";
+  if (*buf == NULL) *buf = new T[n];
+  if (offset != 0)
+    CHECK_EQ(fseek(fp, offset*sizeof(T), SEEK_SET),0) << "fseek failed";
+  size_t ret = fread(*buf,sizeof(T),n,fp);
+  fclose(fp);
+  CHECK_EQ(ret, n) << "read " << filename << " failed";
+  return ret;
+}
+
+template <class T>
+static size_t bin_length(std::string const& filename) {
+  struct stat file_stat;
+  CHECK_EQ(stat(filename.c_str(), &file_stat), 0) << "stat " << filename;
+  return  file_stat.st_size / sizeof(T);
+}
+
+template <class T>
+static size_t load_bin(std::string const& filename, T **buf) {
+  return load_bin<T>(filename, buf, 0, bin_length<T>(filename));
+}
+
+template <class T>
+static size_t save_bin(std::string const& filename, T *buff, size_t n) {
+  FILE *fp = fopen(filename.c_str(), "wb");
+  CHECK_NE(fp, NULL) << "open " << filename << " for writing failed";
+  size_t ret = fwrite(buff,sizeof(T),n,fp);
+  CHECK_EQ(ret, n) << "write " << filename << " failed";
+  fclose(fp);
+  return ret;
+}
+
+}
